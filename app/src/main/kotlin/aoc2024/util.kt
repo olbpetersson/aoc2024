fd99779b292af7c14fdd97ebc5ae1c64088edabc
Ola Petersson <olbpetersson@gmail.com>
2024-12-07 21:11:53 +0100

    day3 p1

--- app/src/main/kotlin/aoc2024/util.kt
+++ app/src/main/kotlin/aoc2024/util.kt
@@ -5,5 +5,7 @@ import java.io.File
 fun String.readFile(): File =
     File(object {}.javaClass.getResource(this)!!.file)
 
-fun String.readFileText(): String =
-    object {}.javaClass.getResource(this)!!.readText().trim()
+fun String.readFileText(
+    overrideValue: String? = null
+): String =
+    overrideValue ?: File(object {}.javaClass.getResource(this)!!.file).readText().trim()
