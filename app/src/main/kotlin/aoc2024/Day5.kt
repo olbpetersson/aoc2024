3aeaa594e955be90c038337f94f8cc977899fe3b
Ola Petersson <olbpetersson@gmail.com>
2024-12-17 20:59:26 +0100

    day5 part2

--- app/src/main/kotlin/aoc2024/Day5.kt
+++ app/src/main/kotlin/aoc2024/Day5.kt
@@ -1,6 +1,6 @@
 package aoc2024
 
-fun String.part5(): Int {
+fun String.day5(): Int {
     val (rulesPart, instructionsPart) = this.split("\n\n")
     val ruleMap = rulesPart.lines()
         .map {
@@ -18,12 +18,50 @@ fun String.part5(): Int {
             val isOk = referencedList?.let {
                 referencedList.none { mutableList.contains(it) }
             }
-            if(isOk == false) {
+            if (isOk == false) {
                 return@filter false
             }
         }
         true
     }.sumOf {
-        it[it.size/2]
+        it[it.size / 2]
     }
+}
+
+fun String.day5Part2(): Int {
+    val (rulesPart, instructionsPart) = this.split("\n\n")
+    val ruleMap = rulesPart.lines()
+        .map {
+            val (first, second) = it.split("|")
+            first.toInt() to second.toInt()
+        }.groupBy({ it.first }, { it.second })
+
+    val faultyLines = instructionsPart.lines().map {
+        it.split(',').map { it.toInt() }
+    }.filter { numberList ->
+        val mutableList = numberList.toMutableList()
+        while (mutableList.isNotEmpty()) {
+            val key = mutableList.removeLast()
+            val referencedList = ruleMap[key]
+            val isOk = referencedList?.let {
+                referencedList.none { mutableList.contains(it) }
+            }
+            if (isOk == false) {
+                return@filter true
+            }
+        }
+        false
+    }
+
+    val sortedLines = faultyLines.map { numberList ->
+        numberList.sortedWith({ x, y ->
+            when {
+                ruleMap[x]?.contains(y) == true -> -1
+                ruleMap[y]?.contains(x) == true -> 1
+                else -> 0
+            }
+        })
+    }.map { it[it.size / 2] }.sumOf { it }
+
+    return sortedLines
 }
--- app/src/test/kotlin/aoc2024/Day5Test.kt
+++ app/src/test/kotlin/aoc2024/Day5Test.kt
@@ -32,7 +32,7 @@ class Day5Test {
 
 75,47,61,53,29
         """.trimIndent()
-        val actual = input.part5()
+        val actual = input.day5()
         assertEquals(61, actual)
     }
 
@@ -63,7 +63,7 @@ class Day5Test {
 
 61,13,29
 """.trimIndent()
-        val actual = input.part5()
+        val actual = input.day5()
         assertEquals(0, actual)
     }
 
@@ -99,12 +99,85 @@ class Day5Test {
 61,13,29
 97,13,75,29,47
 """.trimIndent()
-        val actual = input.part5()
+        val actual = input.day5()
         assertEquals(143, actual)
     }
 
     @Test
     fun `part5 file example`() {
-        assertEquals(5639, "/day5".readFileText().part5())
+        assertEquals(5639, "/day5".readFileText().day5())
     }
+
+    @Test
+    fun `part5 part 2 file example`() {
+        assertEquals(5273, "/day5".readFileText().day5Part2())
+    }
+
+    @Test
+    fun `should fail but be corrected`(): Unit {
+        val input = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+61,13,29
+""".trimIndent()
+        val actual = input.day5Part2()
+        assertEquals(29, actual)
+    }
+
+    @Test
+    fun `aoc example p2`() {
+        val input = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+""".trimIndent()
+        val actual = input.day5Part2()
+        assertEquals(123, actual)
+    }
+
 }
